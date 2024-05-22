c35fa83c804e1220a733ca29611ef1ef8f7eab4e
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-22 01:53:30 +0100

    loading

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,32 @@
+import Loading from "@/components/Loading";
+import { Movie } from "@/types";
 import Image from "next/image";
+import { useState } from "react";
 
-export default function Home() {
+interface Props {
+  netflixOriginals: Movie[];
+  trendingNow: Movie[];
+  topRated: Movie[];
+  actionMovies: Movie[];
+  comedyMovies: Movie[];
+  horrorMovies: Movie[];
+  romanceMovies: Movie[];
+  documentaries: Movie[];
+}
+
+export default function Home({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}: Props) {
   return (
-    <main className="flex min-h-screen h-[200vh] flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Loading />
+    </main>
   );
 }
---
+++ src/components/Loading.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React, { useState } from "react";
+import DotLoader from "react-spinners/DotLoader";
+export default function Loading() {
+//   const [loading, setLoading] = useState(isLoading);
+  return <DotLoader color="red" />;
+}
