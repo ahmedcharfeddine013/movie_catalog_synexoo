ef37a326e648efb277316583b47fd9d1bb3123d8
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-22 04:10:05 +0100

    landing page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import Banner from "@/components/Banner";
 import Loading from "@/components/Loading";
 import { Movie } from "@/types";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Row from "../../components/Row";
+import { fetchTrendingNow } from "@/lib/actions/movies/fetchMovies";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -27,9 +30,10 @@ export default function Home({
   trendingNow,
 }: Props) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <section>
-        <Row />
+        <Banner />
+        {/* <Row /> */}
         {/* {JSON.stringify(actionMovies)} */}
       </section>
     </main>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,35 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "./Logo";
 import { motion, useScroll, useSpring } from "framer-motion";
 
 export default function Navbar() {
-  const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <motion.nav
       initial={{ y: -50, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
-      className="flex items-center justify-center w-full py-5 fixed z-50"
+      className={`flex items-center justify-center w-full py-5 fixed z-50 ${
+        isScrolled ? "bg-background" : ""
+      }`}
     >
       <Logo />
     </motion.nav>
