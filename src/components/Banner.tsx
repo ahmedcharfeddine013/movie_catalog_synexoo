dc468e343a523a790a56927dc1867cdd3521c36d
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-26 15:39:10 +0100

    banner animation

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -13,6 +13,8 @@ import { Play } from "lucide-react";
 import MovieKeywords from "./MovieKeywords";
 import { useRecoilState } from "recoil";
 import { modalState, movieState } from "@/atoms/modalAtoms";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
 
 export default function Banner() {
   const [movie, setMovie] = useState<Movie>();
@@ -23,17 +25,38 @@ export default function Banner() {
     fetchMovie("693134").then((data) => setMovie(data));
   });
 
+  useGSAP(() => {
+    gsap.to("#img-banner", {
+      opacity: 1,
+      delay: 0.5,
+      scale: 1,
+    });
+    gsap.to("#movie-title", {
+      opacity: 1,
+      delay: 0.3,
+      scale: 1,
+    });
+    gsap.to("#movie-release-date", {
+      opacity: 1,
+      delay: 0.8,
+    });
+  }, [movie]);
+
   if (!movie)
     return (
       <div className="flex w-full h-screen items-center justify-center">
         <Loading />
       </div>
     );
+
   return (
     <div
-      className={`${outfit.className} relative flex w-full h-screen items-center justify-center`}
+      className={`${outfit.className} relative flex w-full h-screen items-center justify-center overflow-hidden`}
     >
-      <div className="absolute z-[-50] top-0 left-0 w-full h-full">
+      <div
+        id="img-banner"
+        className="absolute z-[-50] opacity-0 scale-150 top-0 left-0 w-full h-full"
+      >
         <Image
           src={`https://image.tmdb.org/t/p/original${
             movie.backdrop_path || movie.poster_path
@@ -47,10 +70,15 @@ export default function Banner() {
       <div className="absolute bottom-0 bg-gradient-to-t z-[-30] from-background to-transparent h-[50%] w-full"></div>
       <div className="flex flex-col items-center justify-center gap-5">
         <div className="flex flex-col items-center justify-center gap-6">
-          <h1 className="text-4xl lg:text-[8rem]">
+          <h1
+            className="text-4xl lg:text-[8rem] scale-50 opacity-0 "
+            id="movie-title"
+          >
             {movie.name || movie.title}
           </h1>
-          <h3 className="text-xl lg:text-2xl">{movie.release_date}</h3>
+          <h3 className="text-xl lg:text-2xl opacity-0" id="movie-release-date">
+            {movie.release_date}
+          </h3>
           <div className="w-[80%]">
             <MovieKeywords id={movie.id.toString()} />
           </div>
--- src/components/MovieKeywords.tsx
+++ src/components/MovieKeywords.tsx
@@ -4,19 +4,32 @@ import { fetchKeywords } from "@/lib/actions/movies/fetchMovies";
 import { KeywordsProps } from "@/types";
 import React, { useEffect, useState } from "react";
 import Loading from "./Loading";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 export default function MovieKeywords({ id }: { id: string }) {
   const [keyWords, setKeywords] = useState<KeywordsProps[]>();
   useEffect(() => {
-    fetchKeywords(id).then((data) => setKeywords(data.slice(0,5)));
+    fetchKeywords(id).then((data) => setKeywords(data.slice(0, 5)));
   });
+
+  useGSAP(() => {
+    gsap.to(".key-words", {
+      opacity: 1,
+      delay: 0.2,
+      x: 0,
+      stagger: 0.2,
+      ease: "power3.out",
+    });
+  }, [keyWords]);
+
   if (!keyWords) return <Loading />;
   return (
     <div className="flex items-center flex-wrap justify-center gap-4 flex-row">
       {keyWords.map((keyword) => (
         <div
           key={keyword.id}
-          className="bg-white text-primary rounded-none  px-3 py-2"
+          className="bg-white key-words opacity-0 text-primary rounded-none -translate-x-28  px-3 py-2"
         >
           {keyword.name}
         </div>
